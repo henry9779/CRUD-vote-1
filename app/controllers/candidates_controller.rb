55ee05175859399ea9e2dc31d9ce0386230ad5d8
Henry <vinsen1109@gmail.com>
2021-01-09 16:41:11 +0800

    add candidate list in index & add create action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,25 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
   end
 
   def new
     @candidate = Candidate.new
+  end
+
+  def create
+    find_candidate
+
+    if @candidate.save
+      redirect_to root_path, notice: '新增候選人成功'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def find_candidate
+    @candidate = Candidate.new(params[:id])
   end
 end
